2e538daea14626451e90c9dc2bf72d40ec65d2bd
Duke Opoku-Amankwa <jojoamankwa@gmail.com>
2020-12-28 04:26:42 +0000

    Added API Key

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import LinkInput from './components/LinkInput/LinkInput';
 import FaceRecogBox from './components/FaceRecogBox/FaceRecogBox';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '8e2353e633e346fc8fe9412deb92ca7a'
+  });
 
 class App extends Component {
   // Class State
@@ -14,7 +19,16 @@ class App extends Component {
   }
 
   onBtnSubmit = () => {
-    console.log('click');
+    // Detect image
+    app.models.predict({id:'MODEL_ID', version:'MODEL_VERSION_ID'}, "https://samples.clarifai.com/metro-north.jpg")
+    .then(function(response) {
+      // do something with response
+    },
+    function(err) {
+      // there was an error
+      alert('There was an error');
+    }
+  );
   }
 
   render(){
