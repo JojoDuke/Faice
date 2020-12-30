dfc31266ffcc9146972a2680d89ce87eb6fd5902
Duke Opoku-Amankwa <jojoamankwa@gmail.com>
2020-12-30 23:50:17 +0000

    Fixed uploading problem

--- src/App.js
+++ src/App.js
@@ -1,56 +1,61 @@
-import React, { Component } from 'react';
-import Navigation from './components/Navigation/Navigation';
-import LinkInput from './components/LinkInput/LinkInput';
-import FaceRecogBox from './components/FaceRecogBox/FaceRecogBox';
-import Clarifai from 'clarifai';
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import LinkInput from "./components/LinkInput/LinkInput";
+import FaceRecogBox from "./components/FaceRecogBox/FaceRecogBox";
+import Clarifai from "clarifai";
 
 const app = new Clarifai.App({
-  apiKey: '8e2353e633e346fc8fe9412deb92ca7a'
-  });
+  apiKey: "8e2353e633e346fc8fe9412deb92ca7a"
+});
 
 class App extends Component {
   // Class State
   state = {
-    input: '',
-    imageUrl: '',
+    input: "",
+    imageUrl: "",
     isDivVisible: true,
     box: {}
-  }
+  };
 
   calculateFaceLocation = (data) => {
 
-  }
+  };
 
   onInputChange = (e) => {
     this.setState({
+      ...this.state,
       input: e.target.value
     });
-  }
+  };
 
   onBtnSubmit = () => {
-    // Change state
-    this.setState({
-      imageUrl: this.state.input
-    });
+    if (this.state.input !== ""){
+      // Change state
+      this.setState({
+        ...this.state,
+        imageUrl: this.state.input,
+        isDivVisible: false
+      });
+    }
 
     // Detect image
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(function(response) {
-      //console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      this.calculateFaceLocation(response);
-    },
-    function(err) {
-      alert('There was an error');
-    }
-  );
-  }
+      .then(response => this.calculateFaceLocation(response))
+      .catch(err => alert(err)); /* PLEASE COME BACK TO CUSTOMIZE THIS */
+  };
 
-  render(){
-    return(
+  render() {
+    return (
       <div className="App">
-        <Navigation/>
-        <LinkInput onInputChange={this.onInputChange} onBtnSubmit={this.onBtnSubmit}/>
-        <FaceRecogBox imageUrl={this.state.imageUrl}/>
+        <Navigation />
+        <LinkInput
+          onInputChange={this.onInputChange}
+          onBtnSubmit={this.onBtnSubmit}
+        />
+        <FaceRecogBox
+          imageUrl={this.state.imageUrl}
+          isDivVisible={this.state.isDivVisible}
+        />
       </div>
     );
   }
--- src/components/FaceRecogBox/FaceRecogBox.js
+++ src/components/FaceRecogBox/FaceRecogBox.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../FaceRecogBox/FaceRecogBox.css'
 
-const FaceRecogBox = ({ imageUrl }) => {
+const FaceRecogBox = ({ imageUrl, isDivVisible }) => {
     return (
         <div>
-            <div className="main-container">
+            <div className={`main-container ${!isDivVisible && "vanish"}`}>
                 <div className="brokenlines-container">
                     <h3>Drag and drop to upload <br/> Or <a href="/">browse</a> this device</h3>
                 </div>
